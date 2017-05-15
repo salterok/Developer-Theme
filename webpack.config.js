cf0a9773effe45861b8a76e8b211082d67af7966
Sergiy Samborskiy <salterok@gmail.com>
2017-05-15 03:02:44 +0300

    Initial preparation for portfolio project
    Move project under webpack.
    Add package json and customize gitignore.

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("webpack-html-plugin");
+
+module.exports = function(args) {
+
+    args = args || {};
+
+
+    const styles = args.production ? {
+        test: /\.s?css$/, 
+        exclude: /node_modules/, 
+        loader: ExtractTextPlugin.extract(
+            { fallbackLoader: "style-loader", loader: [ "css-loader", "postcss-loader", "sass-loader" ] }
+        )
+    } : {
+        test: /\.s?css$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ]
+    };
+
+    return {
+        context: path.resolve(__dirname, "."),
+        output: {
+            path: path.resolve(__dirname, "../build"),
+            filename: "[name].js",
+            publicPath: "/"
+        },
+        entry: "index.js",
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    query: {
+                        presets: [ "babel-preset-es2015" ]
+                    }
+                },
+                { test: /\.json$/, loader: "json-loader" },
+                { test: /\.(woff|woff2|ttf|eot|mp3|ctm|mp4|bmp|gif|png|je?pg)(\?.*)?$/, loader: "file-loader", query: {
+                    name: "assets/files/[name]_[sha1:hash:base26:7].[ext]"
+                } },
+                { test: /\.ejs$/, loader: "ejs-loader" },
+                styles,
+            ]
+        },
+        resolve: {
+            modules: [
+                ".",
+                "node_modules",
+            ],
+            extensions: [".json", ".js", ".jsx", ".scss", ".html"]
+        },
+        devtool: args.production ? "cheap-source-map" : "inline-source-map",
+        devServer: {
+            contentBase: "./build",
+            inline: true,
+            hot: true,
+        },
+        plugins: [
+            // new CommonsChunkPlugin({
+            //     name: "common",
+            //     filename: "[name].js",
+            //     minChunks: Infinity
+            // }),
+
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: "index.ejs",
+                chunks: [ "common", "ScotlandYard" ],
+                cache: false,
+                hash: true
+            }),
+        ]
+    };
+}
